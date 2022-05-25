6caa73838cfd92f717830a6444c48888cfaafff6
Bo-Hao <bohaoperng@gmail.com>
2022-05-25 16:13:37 +0800

    20220525

--- local_orderbook.go
+++ local_orderbook.go
@@ -2,7 +2,6 @@ package max_RESTfulAPI
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"strings"
--- max_api.go
+++ max_api.go
@@ -2,7 +2,6 @@ package max_RESTfulAPI
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
