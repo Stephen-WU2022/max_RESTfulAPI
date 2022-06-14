e605ec1417107d351ac86d1e779789cabecc8080
Bo-Hao <bohaoperng@gmail.com>
2022-06-14 16:29:47 +0800

    20220614 try sync.Map mapbook

--- log.go
+++ log.go
@@ -8,28 +8,6 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func LogDebugToDailyLogFile(args ...interface{}) error {
-	today := time.Now().Format("2006-01-02")
-	var buffer bytes.Buffer
-	buffer.WriteString("log/")
-	buffer.WriteString(today)
-	buffer.WriteString(".log")
-	file, err := os.OpenFile(buffer.String(), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		return err
-	}
-
-	logrus.SetOutput(file)
-	formatter := &logrus.TextFormatter{
-		FullTimestamp:   true,
-		TimestampFormat: "2006-01-02 15:04:05.999",
-	}
-	logrus.SetFormatter(formatter)
-	logrus.Debug(args)
-	file.Close()
-	return nil
-}
-
 func LogInfoToDailyLogFile(args ...interface{}) error {
 	today := time.Now().Format("2006-01-02")
 	var buffer bytes.Buffer
@@ -74,28 +52,6 @@ func LogWarningToDailyLogFile(args ...interface{}) error {
 	return nil
 }
 
-func LogErrorToDailyLogFile(args ...interface{}) error {
-	today := time.Now().Format("2006-01-02")
-	var buffer bytes.Buffer
-	buffer.WriteString("log/")
-	buffer.WriteString(today)
-	buffer.WriteString(".log")
-	file, err := os.OpenFile(buffer.String(), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		return err
-	}
-
-	logrus.SetOutput(file)
-	formatter := &logrus.TextFormatter{
-		FullTimestamp:   true,
-		TimestampFormat: "2006-01-02 15:04:05.999",
-	}
-	logrus.SetFormatter(formatter)
-	logrus.Error(args)
-	file.Close()
-	return nil
-}
-
 func LogFatalToDailyLogFile(args ...interface{}) error {
 	today := time.Now().Format("2006-01-02")
 	var buffer bytes.Buffer
@@ -114,28 +70,6 @@ func LogFatalToDailyLogFile(args ...interface{}) error {
 	}
 	logrus.SetFormatter(formatter)
 	logrus.Fatal(args)
-	file.Close()
-	return nil
-}
-
-func LogPanicToDailyLogFile(args ...interface{}) error {
-	today := time.Now().Format("2006-01-02")
-	var buffer bytes.Buffer
-	buffer.WriteString("log/")
-	buffer.WriteString(today)
-	buffer.WriteString(".log")
-	file, err := os.OpenFile(buffer.String(), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		return err
-	}
-
-	logrus.SetOutput(file)
-	formatter := &logrus.TextFormatter{
-		FullTimestamp:   true,
-		TimestampFormat: "2006-01-02 15:04:05.999",
-	}
-	logrus.SetFormatter(formatter)
-	logrus.Panic(args)
 	file.Close()
 	return nil
 }
