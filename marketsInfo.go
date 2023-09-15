be811f024c22309e9a2944936a57e3faa337263c
Stephen-WU2022 <tama82045@gmail.com>
2023-09-15 16:32:44 +0800

    add max market info

--- marketsInfo.go
+++ marketsInfo.go
@@ -1,1 +1,46 @@
 package max_RESTfulAPI
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// MaxMarketInfo represents the data structure of a market.
+type MaxMarketInfo struct {
+	ID                 string  `json:"id"`
+	Name               string  `json:"name"`
+	MarketStatus       string  `json:"market_status"`
+	BaseUnit           string  `json:"base_unit"`
+	BaseUnitPrecision  int     `json:"base_unit_precision"`
+	MinBaseAmount      float64 `json:"min_base_amount"`
+	QuoteUnit          string  `json:"quote_unit"`
+	QuoteUnitPrecision int     `json:"quote_unit_precision"`
+	MinQuoteAmount     float64 `json:"min_quote_amount"`
+	MWalletSupported   bool    `json:"m_wallet_supported"`
+}
+
+// GetMaxMarketInfo fetches market information from the API.
+func GetMaxMarketInfo() ([]MaxMarketInfo, error) {
+	// Define the API URL.
+	apiURL := "https://max-api.maicoin.com/api/v2/markets"
+
+	// Make an HTTP GET request to the API.
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Check the response status code.
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(" Max market info API request failed with status code: %d", resp.StatusCode)
+	}
+
+	// Decode the JSON response into a slice of MaxMarketInfo.
+	var response []MaxMarketInfo
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
